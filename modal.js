10da7d30c8dde229c28824b201b6a6a7bf4d28d0
Pinyin <kevin101zhang@gmail.com>
2019-02-25 18:12:41 -0500

    Nearly Done

--- modal.js
+++ modal.js
@@ -2,19 +2,54 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var ScrapeSchema = new Schema({
+    title: {
+        type: String,
+        trim: true,
+    },
     link: {
         type: String,
         trim: true,
     },
-    title: {
-        type: String,
-        trim: true,
-
-    },
     summary: {
         type: String,
         trim: true,
-    }
+    },
+    tags: {
+        type: String,
+        trim: true,
+    },
+    relativeTime: {
+        type: String,
+        trim: true,
+    },
+    absoluteTime: {
+        type: String,
+        trim: true,
+    },
+    userProfileLink: {
+        type: String,
+        trim: true,
+    },
+    userName: {
+        type: String,
+        trim: true,
+    },
+    userReputationScore: {
+        type: String,
+        trim: true,
+    },
+    views: {
+        type: String,
+        trim: true,
+    },
+    votes: {
+        type: String,
+        trim: true,
+    },
+    answers: {
+        type: String,
+        trim: true,
+    },
 });
 
 var ScrapedModal = mongoose.model("ScrapedModal", ScrapeSchema);
--- server.js
+++ server.js
@@ -4,39 +4,76 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var exphbs = require("express-handlebars");
-
 var PORT = 3000;
-
 var db = require("./modal");
-
 var app = express();
+// var path = require("path");
+// var fs = require('fs');
 
 app.use(logger("dev"));
 app.use(express.urlencoded({
     extended: true
 }));
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.static("public"));
+
 
 mongoose.connect("mongodb://localhost/ScraperDatabase", {
     useNewUrlParser: true
 });
 
-app.get("/scrape", function (req, res) {
+app.get("/", function (req, res) {
 
-    axios.get("https://old.reddit.com/r/webdev").then(function (response) {
+
+    axios.get("https://stackoverflow.com/questions/tagged/react").then(function (response) {
 
         var $ = cheerio.load(response.data);
 
-        $("p.title").each(function (i, element) {
+        $("div.summary").each(function (i, element) {
 
             var result = {};
 
-            result.title = $(this).text();
-            result.link = $(this).children().attr("href");
-            result.summary = 
+            // Title
+            result.title = $(this).find("h3").text();
+
+            // Link
+            var link = $(this).children("h3").find("a").attr("href");
+            result.link = "https://stackoverflow.com" + link;
+
+            // Summary
+            result.summary = $(this).find("div.excerpt").text();
+
+            // Tags
+            result.tags = $(this).find("div.tags").text();
+
+            // Relative Time
+            result.relativeTime = $(this).find("div.user-info ").find("div.user-action-time").text();
+
+            // Absolute Time
+            result.absoluteTime = $(this).find("div.user-info ").find("div.user-action-time").find("span").attr("title");
+
+            // User Profile Link
+            userProfileLink = $(this).find("div.user-info ").find("div.user-gravatar32").find("a").attr("href");
+            result.userProfileLink = "https://stackoverflow.com" + userProfileLink;
+
+            // User Name
+            result.userName = $(this).find("div.user-info ").find("div.user-details").find("a").text();
+
+            // User Reputation Score
+            result.userReputationScore = $(this).find("div.user-info ").find("div.user-details").find("div.-flair").find("span.reputation-score").text();
+
+            // Views
+            result.views = $(this).prev().find("div.views").text();
+
+            // Votes
+            result.votes = $(this).prev().find("div.stats").find("div.vote").find("div.votes").find("span.vote-count-post").text();
+
+            // Answers
+            result.answers = $(this).prev().find("div.stats").find("div.status").find("strong").text();
 
             db.create(result)
                 .then(function (addedResult) {
@@ -48,24 +85,22 @@ app.get("/scrape", function (req, res) {
                     console.log(err);
                 });
         });
-        res.send("Scrape Complete");
     });
-});
 
-app.get("/", function(req, res) {
-    // Find all ScrapedModals
     db.find({})
-      .then(function(ResultingInfo) {
-        // If all ScrapedModals are successfully found, send them back to the client
+        .then(function (ResultingInfo) {
+            // If all ScrapedModals are successfully found, send them back to the client
 
-        // res.json(ResultingInfo);
-        res.render("index", { scrapedmodals: ResultingInfo });
-      })
-      .catch(function(err) {
-        // If an error occurs, send the error back to the client
-        res.json(err);
-      });
-  });
+            // res.json(ResultingInfo);
+            res.render("index", {
+                scrapedmodals: ResultingInfo
+            });
+        })
+        .catch(function (err) {
+            // If an error occurs, send the error back to the client
+            res.json(err);
+        });
+});
 
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
